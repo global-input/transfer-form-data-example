25732908ec9753ec060377c3053e6c32b6948b3d
Dilshat Hewzulla <hewzulla@gmail.com>
2020-11-25 15:37:40 +0000

    code organisation

--- src/app/mobile/index.tsx
+++ src/app/mobile/index.tsx
@@ -2,18 +2,11 @@ import React, { useCallback } from 'react';
 import * as globalInput from 'global-input-react';////global-input-react////
 
 ////main////
-
 import * as storage from '../storage';
 
-import { AppContainer, RowCenter, FormContainer, TextButton, QRCodeContainer, DisplayErrorMessage, MessageContainer } from '../app-layout';
+export * from 'global-input-react';////global-input-react////
 
-interface ControlledContainerProps {
-    domain: string;
-    title: string;
-    notConnected?: React.ReactNode;
-    errorMessage?: string;
-}
-export const useMobile = (initData: globalInput.InitData | (() => globalInput.InitData)) => {
+export const useMobile = (initData: globalInput.InitData | (() => globalInput.InitData), connect: boolean = true) => {
     const connectionSettings = storage.loadConnectionSettings();
     const options: globalInput.ConnectOptions = {
         url: connectionSettings.url,////use your own server"
@@ -22,47 +15,48 @@ export const useMobile = (initData: globalInput.InitData | (() => globalInput.In
     };
     const mobile = globalInput.useGlobalInputApp({
         initData, options, codeAES: connectionSettings.codeKey
-    });
-
-    const setOnFieldChange = useCallback((onFieldChange: (field: globalInput.FormField) => void) => {
-        mobile.setOnchange(({ field }) => {
-            onFieldChange(field);
-        })
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [mobile.setOnchange]);
-
+    }, connect);
     ////dev-test codeData
-
-
-
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    const restart = useCallback(() => mobile.restart(), [mobile.restart]);
-
-    const disconnectButton = (<TextButton onClick={restart} label="Disconnect" />);
-
-
-    const ControlledContainer: React.FC<ControlledContainerProps> = useCallback(({ domain, title, notConnected, errorMessage, children }) => (
-        <AppContainer title={title} domain={domain}>
-            {mobile.isConnectionDenied && (
-                <FormContainer>
-                    <MessageContainer>You can only use one mobile app per session. Disconnect to start a new session.</MessageContainer>
-                    <RowCenter>
-
-                        {disconnectButton}
-                    </RowCenter>
-                </FormContainer>
-
-            )}
-            {mobile.isReady && (<QRCodeContainer><mobile.ConnectQR /></QRCodeContainer>)}
-            {(mobile.isError || errorMessage) ? (<DisplayErrorMessage errorMessage={errorMessage ? errorMessage : mobile.errorMessage} />) : (mobile.isConnected && children)}
-            {(!mobile.isConnected) && notConnected}
-
-        </AppContainer>
+    const ConnectQR = useCallback((props: globalInput.ConnectQRProps) => {
+        let errorMessage = mobile.isConnectionDenied && "You can only use one mobile app per session. Disconnect to start a new session.";
+        if (mobile.isError) {
+            errorMessage = mobile.errorMessage;
+        }
+        return (
+            <div>
+                <div style={styles.content}>
+                    <mobile.ConnectQR {...props} />
+                    {errorMessage && (<div style={styles.errorMessage}>{errorMessage}</div>)}
+                </div>
+            </div>
+        );
         // eslint-disable-next-line react-hooks/exhaustive-deps
-    ), [mobile.isConnectionDenied, mobile.isError, mobile.isConnected, mobile.isReady, mobile.disconnect, mobile.ConnectQR, mobile.errorMessage]);
-
-    return { ...mobile, ControlledContainer, disconnectButton, setOnFieldChange };
+    }, [mobile.isError, mobile.isConnectionDenied, mobile.ConnectQR]);
+    return { ...mobile, ConnectQR };
 };
+
+const styles = {
+    container: {
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'center',
+        width: '100%',
+        paddingTop: 30,
+        position: "absolute",
+        zIndex: 100,
+    } as React.CSSProperties,
+    content: {
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'flex-center',
+        alignItems: 'flex-start'
+    } as React.CSSProperties,
+    errorMessage: {
+        color: 'red',
+        fontSize: 11
+    }
+};
+
 
 export const userWithDomainAsFormId = (initData: globalInput.InitData) => {
     if (initData?.form?.domain && initData?.form?.fields?.length) {
@@ -81,5 +75,3 @@ export const userWithDomainAsFormId = (initData: globalInput.InitData) => {
         initData.form.id = `###${textFields[0].id}###@${initData.form.domain}`;
     }
 };
-
-export type { FormField, FieldValue } from 'global-input-react';////global-input-react////

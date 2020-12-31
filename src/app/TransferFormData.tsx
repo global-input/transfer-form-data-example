a000a861a4b9e78dd15e03f9f6f4aefea39a75d7
Dilshat Hewzulla <hewzulla@gmail.com>
2020-12-31 09:52:31 +0000

    syled-components

--- src/app/TransferFormData.tsx
+++ src/app/TransferFormData.tsx
@@ -1,7 +1,8 @@
 import React, { useState, useCallback } from 'react';
 //+//import { useHistory } from 'react-router-dom'; ////website
 import { useMobile, ConnectWidget,FormField, InitData} from './mobile';
-import { AppContainer, FormContainer, DisplayInputCopyField, TextButton, FormFooter} from './app-layout';
+import { FormContainer, DisplayInputCopyField, TextButton, FormFooter} from './app-layout';
+import {AppContainer,Title,Form,ConnectedInstruction,Field,Input,TextArea,Label,CopyToClipboardButton} from './elements';
 //+//import * as mobileUI from '../../micro-apps/mobile-ui'; ////website
 interface Props {
     domain: string;
@@ -66,6 +67,7 @@ const TransferFormData: React.FC<Props> = ({ domain, formFields, setFormFields, 
     });
 
     const onFieldChanged = (formFields: FormField[], formField: FormField, index: number, value: string) => {
+
         const changedFormFields = computeChangedFormFields(formFields, formField.id, value, index);
         if (changedFormFields) {
             setFormFields(changedFormFields);
@@ -74,15 +76,22 @@ const TransferFormData: React.FC<Props> = ({ domain, formFields, setFormFields, 
     }
 
     return (
-        <AppContainer title="Form Data Transfer" domain={domain}>
+        <AppContainer>
+            <Title>{domain}</Title>
+
             <ConnectWidget mobile={mobile}/>
             {mobile.isConnected && (
-                <FormContainer>
-                    {formFields.map((formField, index) => (<DisplayInputCopyField
+                <Form>
+                    {formFields.map((formField, index) => (<DisplayInputField  key={formField.id} index={index}
+                        formField={formField} onFieldChanged={onFieldChanged} formFields={formFields} visibility={visibility}/>
+                ))}
+        {/*
+                    <DisplayInputCopyField
                         field={formField}
                         key={formField.id}
                         hideValue={visibility.value === 0} onChange={value => onFieldChanged(formFields, formField, index, value)} />))}
-                </FormContainer>
+            */}
+                </Form>
             )}
             <FormFooter>
                 <TextButton onClick={editDomain} label="Edit Domain" />
@@ -96,6 +105,27 @@ const TransferFormData: React.FC<Props> = ({ domain, formFields, setFormFields, 
 
 
 };
+const DisplayInputField=({formFields,formField,index,onFieldChanged,visibility})=>{
+
+    if(visibility.value === 0 || (!formField.nLines) || formField.nLines <=1){
+        return (<Field>
+            <Input id={formField.id}  type={visibility.value === 0?'password':'text'} value={formField.value} placeholder={formField.label}
+            onChange={(evt)=>onFieldChanged(formFields, formField, index, evt.target.value)}/>
+            <Label htmlFor="decryptedContent">{formField.label}</Label>
+            <CopyToClipboardButton value={formField.value}>Copy</CopyToClipboardButton>
+            </Field>);
+    }
+    else{
+        return (<Field>
+            <TextArea id={formField.id} value={formField.value} placeholder={formField.label}
+                onChange={(evt)=>onFieldChanged(formFields, formField, index, evt.target.value)}/>
+                <Label htmlFor="decryptedContent">{formField.label}</Label>
+                <CopyToClipboardButton value={formField.value}>Copy</CopyToClipboardButton>
+        </Field>);
+    }
+}
+
+
 
 
 const FIELDS = {
